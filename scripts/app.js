f2e413fb63700db45dcfedc8711272b8a453347d
sabbir <29686831+aboutsabbir@users.noreply.github.com>
2023-02-22 12:28:19 +0600

    solved file upload issue in angular

--- scripts/app.js
+++ scripts/app.js
@@ -33,6 +33,53 @@ app.controller("documentController", function($scope) {
     'ABC Vendor Ltd'
   ];
 
+  $('#dropareaInvoice').on(
+    'drop',
+    function(e) {
+      e.preventDefault();
+      handleFileTransfer(e, 'invoiceFile');
+  });
+
+  $('#dropareaShortFund').on(
+    'drop',
+    function(e) {
+      e.preventDefault();
+      handleFileTransfer(e, 'shortFundFile');
+  });
+
+  $('#dropareaOther').on(
+    'drop',
+    function(e) {
+      e.preventDefault();
+      handleFileTransfer(e, 'othersFile');
+  });
+
+  function handleFileTransfer(event, elementIdName) {
+    if (event.originalEvent.dataTransfer && event.originalEvent.dataTransfer.files.length) {
+      const file = event.originalEvent.dataTransfer.files[0];
+      const reader = new FileReader();
+
+      if (file) {
+        reader.addEventListener("load", ()=>{
+          console.log(`File: ${file.name} read successfully!`);
+          console.log(getFileContent(reader.result));
+        });
+
+        reader.addEventListener("error", ()=>{
+          console.log(`Error occurred reading file: ${file.name}`);
+        })
+      }
+
+      reader.readAsDataURL(file);
+    }
+  }
+
+  // returns the blob data
+  function getFileContent(fileData) {
+    let fileContent = String(fileData);
+    return fileContent.substring(fileContent.indexOf(',') + 1);
+  }
+
   $scope.handleUpload = function(fileType) {
     console.log($scope.invoiceData);
     console.log(fileType);
