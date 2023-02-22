635fe3db5df51738204da34420f702fad44c549a
Sabbir Ahmed <29686831+aboutsabbir@users.noreply.github.com>
2023-02-22 14:04:44 +0600

    file blob upload success

--- scripts/app.js
+++ scripts/app.js
@@ -1,25 +1,30 @@
 const app = angular.module("myapp", []);
 
 app.controller("documentController", function($scope) {
-  $scope.demoDate = new Date().toLocaleDateString();
-  $scope.invoiceData = {}
-  $scope.demo = JSON.stringify($scope.invoiceData);
+  // $scope.demoDate = new Date().toLocaleDateString();
+  $scope.invoiceData = {
+    payableTo: '',
+    fileBlob: null,
+    fileName: null
+  }
 
   $scope.shortFundData = {
     paidTo: '',
-    file: {}
+    fileBlob: null,
+    fileName: null
   }
 
   $scope.othersData = {
     payeeAssociated: '',
-    fileType: '',
-    file: {}
+    fileBlob: null,
+    fileName: null,
+    otherFileType: null
   }
 
-  $scope.uploadedDocInfo = {
-    selectedType: '',
-    payeeAssociated: ''
-  }
+  // $scope.uploadedDocInfo = {
+  //   selectedType: '',
+  //   payeeAssociated: ''
+  // }
 
   $scope.availableTypes = [
     'Invoice',
@@ -37,32 +42,46 @@ app.controller("documentController", function($scope) {
     'drop',
     function(e) {
       e.preventDefault();
-      handleFileTransfer(e, 'invoiceFile');
+      handleFileTransfer(e, 'invoice');
   });
 
   $('#dropareaShortFund').on(
     'drop',
     function(e) {
       e.preventDefault();
-      handleFileTransfer(e, 'shortFundFile');
+      handleFileTransfer(e, 'shortFund');
   });
 
   $('#dropareaOther').on(
     'drop',
     function(e) {
       e.preventDefault();
-      handleFileTransfer(e, 'othersFile');
+      handleFileTransfer(e, 'others');
   });
 
-  function handleFileTransfer(event, elementIdName) {
+  function handleFileTransfer(event, fileType) {
     if (event.originalEvent.dataTransfer && event.originalEvent.dataTransfer.files.length) {
       const file = event.originalEvent.dataTransfer.files[0];
       const reader = new FileReader();
 
       if (file) {
         reader.addEventListener("load", ()=>{
-          console.log(`File: ${file.name} read successfully!`);
-          console.log(getFileContent(reader.result));
+          // console.log(`File: ${file.name} read successfully!`);
+          if (fileType === 'invoice') {
+            $scope.invoiceData.fileBlob = getFileContent(reader.result);
+            $scope.invoiceData.fileName = file.name;
+            console.log($scope.invoiceData);
+          } else if (fileType === 'shortFund') {
+            $scope.shortFundData.fileBlob = getFileContent(reader.result);
+            $scope.shortFundData.fileName = file.name;
+            console.log($scope.shortFundData);
+          } else if (fileType === 'others') {
+            $scope.othersData.fileBlob = getFileContent(reader.result);
+            $scope.othersData.fileName = file.name;
+            console.log($scope.othersData);
+          }
+          
+          $scope.$digest();
         });
 
         reader.addEventListener("error", ()=>{
@@ -78,6 +97,16 @@ app.controller("documentController", function($scope) {
   function getFileContent(fileData) {
     let fileContent = String(fileData);
     return fileContent.substring(fileContent.indexOf(',') + 1);
+  }
+
+  $scope.resetFileBlob = function(fileType) {
+    if (fileType === 'invoice') {
+      $scope.invoiceData = {};
+    } else if (fileType === 'shortFund') {
+      $scope.shortFundData = {};
+    } else {
+      $scope.othersData = {};
+    }
   }
 
   $scope.handleUpload = function(fileType) {
